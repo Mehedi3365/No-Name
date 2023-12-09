0c28b617217514fa78f0dc0198583141eb521c72
Mehedi3365 <mehedihassanemon369@gmail.com>
2023-12-09 13:38:30 +0600

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,38 @@
-// window scroll mathod
-function windowScroll() {
-    const navbar = document.getElementById("navbar2");
-    if (
-        document.body.scrollTop >= 50 ||
-        document.documentElement.scrollTop >= 50
-    ) {
-        navbar.classList.add("is-sticky");
-    } else {
-        navbar.classList.remove("is-sticky");
-    }
-}
-window.addEventListener('scroll', (ev) => {
-    ev.preventDefault();
-    windowScroll();
-})
-
-
-
-
-
 $(document).ready(function() {
+
+    // for testimonial
+    $('.testimonial-box').owlCarousel({
+        items: 3,
+        margin: 30,
+        loop: true,
+        donts: false,
+        nav: false,
+
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1,
+                margin: 10,
+            },
+            479: {
+                items: 1.8,
+                margin: 10,
+
+            },
+            767: {
+                items: 2,
+                margin: 20,
+            },
+            992: {
+                items: 3,
+                margin: 20,
+            },
+
+
+        }
+
+    });
+    // for mobitle menu
     $('.menutreger').click(function() {
         $('.mobile-menu').toggleClass('menuShow');
         $('#navbar2').toggleClass('ismenu');
@@ -36,3 +49,19 @@ $(document).ready(function() {
         fixedContentPos: false,
     });
 });
+// window scroll mathod
+function windowScroll() {
+    const navbar = document.getElementById("navbar2");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add("is-sticky");
+    } else {
+        navbar.classList.remove("is-sticky");
+    }
+}
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
